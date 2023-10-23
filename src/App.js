241fa3ccce76c812c2d2acc3157584e7257ab7a1
Jannatul Ferdaous Lam <lam@itlogiko.com>
2023-10-23 09:51:35 +0600

    - separate test part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
+
 
 function App() {
   return (
     <>
-      {/* <Navbar title="TextUtils" name="hello" /> */}
-      <Navbar title='TextUtils' />
-      <div className='container'>
-        <TextForm header='Enter Your Text To Analyze Below' />
-      </div>
-      <About />
+      
     </>
   );
 }
