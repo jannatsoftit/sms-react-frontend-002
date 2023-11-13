685443a45a25ef959b3d7a45a4095fc06156f7c4
Jannatul Ferdaous Lam <lam@itlogiko.com>
2023-11-13 13:37:08 +0600

    - fixing sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 //import { NavLink } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
-import SubMenu  from './SubMenu';
+import SubMenu from './SubMenu';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { IconContext } from 'react-icons/lib';
@@ -22,39 +22,58 @@ const SidebarNav = styled.nav`
 
 const Sidebar = ({ children }) => {
   const [sidebar, setSidebar] = useState('true');
-  //const showSidebar = () => setSidebar(!sidebar);
 
-  return(
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
     <>
-    <div className='con'>
-    <IconContext.Provider value={{}}>
-      <div>
-        <a href='/'>Hello</a>
-      </div>
-      {/* <div className='nav'>
+      <div className='con'>
+        <IconContext.Provider value={{}}>
+          {/* <div className='nav'>
         <div className='navicon'>
           <FaIcons.FaBars onClick={showSidebar} />
         </div>
       </div> */}
 
-      <SidebarNav sidebar={sidebar}>
-        <div className='sidebar_wrap'>
-          {/* <div className='navicon'>
+          <SidebarNav sidebar={sidebar}>
+            <div className='sidebar_wrap'>
+              <div
+                style={{ width: isOpen ? '250px' : '50px' }}
+                className='sidebar'
+              >
+                <div className='top_section'>
+                  <h1
+                    style={{ display: isOpen ? 'block' : 'none' }}
+                    className='logo'
+                  >
+                    Logo
+                  </h1>
+                  <div
+                    style={{ marginLeft: isOpen ? '100px' : '0px' }}
+                    className='bars'
+                  >
+                    <FaIcons.FaBars onClick={toggle} />
+                  </div>
+                </div>
+
+                {/* <div className='navicon'>
             <AiIcons.AiOutlineClose />
           </div> */}
-          {SidebarData?.map((item, index) => {
-            return <SubMenu item={item} key={index}/>
-          })}
-
-        </div>
-      </SidebarNav>
-      
-    </IconContext.Provider>
-    <main>{children}</main>
-    </div>
-    
+                {SidebarData?.map((item, index) => {
+                  return <SubMenu item={item} key={index} />;
+                })}
+              </div>
+            </div>
+          </SidebarNav>
+        </IconContext.Provider>
+        <main>{children}</main>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Sidebar;
--- src/Components/SubMenu.jsx
+++ src/Components/SubMenu.jsx
@@ -1,42 +1,51 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
 
 const SubMenu = ({ item }) => {
   const [submenu, setSubmenu] = useState(false);
 
   const showSubmenu = () => setSubmenu(!submenu);
 
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <NavLink
-        to={item.path}
-        className='link'
-        onClick={item.subNav && showSubmenu}
-        // id={window.location.pathname == item.path ? 'active' : ''}
-      >
-        <div>
-          <div className='sidebar_icon'>{item.icon}</div>
-          <div className='sidebar_label'>{item.title}</div>
-        </div>
-        <div>
-          
-          {item.subNav && submenu
-            ? item.iconOpen
-            : item.subNav
-            ? item.iconClosed
-            : null}
-        </div>
-      </NavLink>
+     
 
-      {submenu &&
-        item?.subNav.map((item, index) => {
-          return (
-            <a className='dropdown_link' href={item.path} key={index}>
-              <div className='sidebar_icon'>{item.icon}</div>
-              <div className='submenu_label'>{item.title}</div>
-            </a>
-          );
-        })}
+        <NavLink
+          to={item.path}
+          className='link'
+          onClick={item.subNav && showSubmenu}
+          // id={window.location.pathname == item.path ? 'active' : ''}
+        >
+          
+          <div>
+            <div className='sidebar_icon'>{item.icon}</div>
+            <div className='sidebar_label'>{item.title}</div>
+          </div>
+          <div>
+            {item.subNav && submenu
+              ? item.iconOpen
+              : item.subNav
+              ? item.iconClosed
+              : null}
+          </div>
+        </NavLink>
+
+
+        {submenu &&
+          item?.subNav.map((item, index) => {
+            return (
+              <a className='dropdown_link' href={item.path} key={index}>
+                <div className='sidebar_icon'>{item.icon}</div>
+                <div className='submenu_label'>{item.title}</div>
+              </a>
+            );
+          })}
+      
     </>
   );
 };
