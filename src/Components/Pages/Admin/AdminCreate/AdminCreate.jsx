97e06052342f4347eae05e2a591cf16b34f4a8fc
Jannatul Ferdaous Lam <lam@itlogiko.com>
2023-11-14 17:53:32 +0600

    - added back admin table button

--- src/Components/Pages/Admin/AdminCreate/AdminCreate.jsx
+++ src/Components/Pages/Admin/AdminCreate/AdminCreate.jsx
@@ -1,8 +1,18 @@
 import AdminForm from "../../../forms/AdminForm/Adminform";
+import {Link} from 'react-router-dom';
+import {AiOutlineArrowRight} from 'react-icons/ai';
 
 const AdminCreate = () => {
   return (
     <div>
+      {/* Admin create table title section */}
+      <Link to="admins" className="add_button">
+      <button type="button" className="btn">
+        Admin Table  <AiOutlineArrowRight /> 
+      </button>
+      </Link>
+
+      {/* Admin form section */}
       <AdminForm />
     </div>
   )
